422c237b3c355da6a531dab66c2b383498e70574
harsha1010 <harshasrinivas1010@gmail.com>
2023-06-16 19:52:39 +0530

    vhange

--- src/navigation.js
+++ src/navigation.js
@@ -6,7 +6,7 @@ function Navbar() {
 
             <nav class="navbar navbar-expand-lg  sticky-top   ">
                 <div class="container-fluid fixed-top">
-                    <a class="navbar-brand" href="#">Me<span>iu</span>m</a>
+                    <a class="navbar-brand" href="#">TA<span>R</span>AS</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
